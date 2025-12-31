56bd9459dc3c6bc3cfa366a10237b4091f05100a
Devin Checa <devincheca@gmail.com>
2025-12-30 18:51:57 -0800

    ALC folder created

---
+++ alc/index.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const sizeOf = require('image-size')
+
+const manifest = 'manifest.json';
+const imageFolders = ['images', 'digital'];
+
+imageFolders
+  .filter(imageFolder => !imageFolder.includes('resume'))
+  .map(imageFolder => {
+    const filePath = `/${imageFolder}/${manifest}`;
+    const directoryPath = path.join(__dirname, `/${imageFolder}`);
+    fs.unlink(`.${filePath}`, err => {
+      if (err) {
+        console.error(err);
+      }
+    
+      fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+          return console.log('Unable to scan directory: ' + err);
+        }
+    
+        const imageArray = files
+          .filter(fileName => !fileName.includes('json'))
+          .map(fileName => {
+            const { height, width } = sizeOf(`${imageFolder}/${fileName}`);
+            return {
+              fileName,
+              height,
+              width,
+              isNewLine: width > 1.5 * height,
+            };
+          });
+    
+        fs.appendFile(`./${filePath}`, JSON.stringify(imageArray), err => {
+          if (err) throw err;
+          console.log('Image manifest is created successfully.');
+        });
+      });
+    });
+  });
+
